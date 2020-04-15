ee3ee15864b4e84ba8948a8d18f722cf16192e59
Hayley Smith <hayley.j.smith@gmail.com>
2020-04-14 21:28:07 -0500

    final commit

---
+++ src/app/satellite.ts
@@ -0,0 +1,26 @@
+export class Satellite {
+    name: string;
+    orbitType: string;
+    type: string;
+    operational: boolean;
+    launchDate: string;
+    
+    shouldShowWarning(): boolean {
+       if (this.type == "Space Debris") {
+          return true 
+       } else {
+          return false
+       }
+    }
+    constructor(name: string, type: string, launchDate: string, orbitType: string,
+         operational: boolean) {
+            this.name = name;
+            this.orbitType = orbitType;
+            this.type = type;
+            this.operational = operational;
+            this.launchDate = launchDate;
+  
+         }
+         
+
+}
