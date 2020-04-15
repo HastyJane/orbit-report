32e4bd331f4a4e98e265d41371e2e393b9854ce1
Hayley Smith <hayley.j.smith@gmail.com>
2020-04-15 16:52:43 -0500

    final commit

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,5 +1,8 @@
 import { TestBed, async } from '@angular/core/testing';
 import { AppComponent } from './app.component';
+import { Satellite } from './satellite';
+
+
 
 describe('AppComponent', () => {
   beforeEach(async(() => {
